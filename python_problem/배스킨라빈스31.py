70d738e6a933d2d29d0d7e739a2908a02fc11b3e
Shin-myungkyung <shin1112@gmail.com>
2025-01-04 02:57:08 +0900

    5단계

--- python_problem/배스킨라빈스31.py
+++ python_problem/배스킨라빈스31.py
@@ -19,3 +19,24 @@ while True:
 for i in range(a):
     num+=1
     print(f"playerA : {num}")
+
+
+while True:
+    b = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+
+    try:
+        b = int(b)  
+    except ValueError:
+        print("정수를 입력하세요")
+        continue 
+
+    if b != 1 and b != 2 and b != 3:
+        print("1, 2, 3 중 하나를 입력하세요")
+        continue 
+
+    else:
+        break  
+
+for i in range(b):
+    num+=1
+    print(f"playerB : {num}")

62c71be5de5b3fa033fbd0158b958ba72afacc68
Shin-myungkyung <shin1112@gmail.com>
2025-01-04 03:08:21 +0900

    6단계

--- python_problem/배스킨라빈스31.py
+++ python_problem/배스킨라빈스31.py
@@ -1,42 +1,55 @@
 num = 0
 
-while True:
-    a = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
-    try:
-        a = int(a)  
-    except ValueError:
-        print("정수를 입력하세요")
-        continue 
+while True: 
+    while True:
+        a = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
-    if a != 1 and a != 2 and a != 3:
-        print("1, 2, 3 중 하나를 입력하세요")
-        continue 
+        try:
+            a = int(a)  
+        except ValueError:
+            print("정수를 입력하세요")
+            continue 
 
-    else:
-        break  
+        if a != 1 and a != 2 and a != 3:
+            print("1, 2, 3 중 하나를 입력하세요")
+            continue 
 
-for i in range(a):
-    num+=1
-    print(f"playerA : {num}")
+        else:
+            break  
 
+    for i in range(a):
+        num+=1
+        if num == 32:
+            break
+        else:
+            print(f"playerA : {num}")
 
-while True:
-    b = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+    if num == 32:
+        break
 
-    try:
-        b = int(b)  
-    except ValueError:
-        print("정수를 입력하세요")
-        continue 
+    while True:
+        b = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
-    if b != 1 and b != 2 and b != 3:
-        print("1, 2, 3 중 하나를 입력하세요")
-        continue 
+        try:
+            b = int(b)  
+        except ValueError:
+            print("정수를 입력하세요")
+            continue 
 
-    else:
-        break  
+        if b != 1 and b != 2 and b != 3:
+            print("1, 2, 3 중 하나를 입력하세요")
+            continue 
 
-for i in range(b):
-    num+=1
-    print(f"playerB : {num}")
+        else:
+            break  
+
+    for i in range(b):
+        num+=1
+        if num == 32:
+            break
+        else:
+            print(f"playerB : {num}")
+
+    if num == 32:
+        break

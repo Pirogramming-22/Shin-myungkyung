d2d79b69dee1a92e725d1bf78ab2ddb372710faf
Shin-myungkyung <shin1112@gmail.com>
2025-01-04 03:27:07 +0900

    6단계 수정

--- python_problem/배스킨라빈스31.py
+++ python_problem/배스킨라빈스31.py
@@ -2,6 +2,7 @@ num = 0
 
 
 while True: 
+    #player A
     while True:
         a = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
@@ -10,24 +11,22 @@ while True:
         except ValueError:
             print("정수를 입력하세요")
             continue 
-
         if a != 1 and a != 2 and a != 3:
             print("1, 2, 3 중 하나를 입력하세요")
             continue 
-
         else:
             break  
 
     for i in range(a):
         num+=1
-        if num == 32:
+        print(f"playerA : {num}")
+        if num == 313:
             break
-        else:
-            print(f"playerA : {num}")
 
-    if num == 32:
+    if num == 31:
         break
 
+    #player B
     while True:
         b = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
@@ -36,20 +35,17 @@ while True:
         except ValueError:
             print("정수를 입력하세요")
             continue 
-
         if b != 1 and b != 2 and b != 3:
             print("1, 2, 3 중 하나를 입력하세요")
             continue 
-
         else:
             break  
 
     for i in range(b):
         num+=1
-        if num == 32:
+        print(f"playerB : {num}")
+        if num == 31:
             break
-        else:
-            print(f"playerB : {num}")
 
-    if num == 32:
+    if num == 31:
         break
